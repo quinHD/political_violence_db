4a9c6b88e29c6cf6111223ab76c3e73b890bc215
quinTango <quino@acidtango.com>
2017-11-28 01:02:54 +0100

    New Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true 
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-
+set :bundle_binstubs, nil
 ## Defaults:
 # set :scm,           :git
 # set :branch,        :master
@@ -38,7 +38,7 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 
 ## Linked Files & Directories (Default None):
 set :linked_files, %w(config/database.yml config/secrets.yml)
-set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs,  %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
